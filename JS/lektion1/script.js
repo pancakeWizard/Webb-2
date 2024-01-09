9355e36656e8d94beec896d40466c96e8f721ab6
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-01-09 11:50:41 +0100

    js-1

---
+++ JS/lektion1/script.js
@@ -0,0 +1,17 @@
+let textToChange = document.querySelector('.mainTitle')
+textToChange.innerHTML = "I changed you"
+
+let mha = document.querySelector('h2')
+mha.innerHTML = "Ändrade mha taggen"
+
+let blockEl = document.querySelector('#myId')
+let pToAppend = document.createElement("p")
+pToAppend.innerHTML = "Ett nytt barn har fötts"
+blockEl.appendChild(pToAppend)
+
+let headerToChange = document.querySelector("#barn")
+headerToChange.innerHTML = "hej"
+
+mha.remove();
+
+blockEl.removeChild(headerToChange)
